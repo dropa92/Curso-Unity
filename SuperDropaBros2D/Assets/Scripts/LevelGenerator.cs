ec3718d796b29382ee5b9db71e6133b68b10f1cd
dropa92 <dropa_1@hotmail.com>
2020-10-09 20:56:45 +0100

    Update | 09 - 10 - 2020

---
+++ SuperDropaBros2D/Assets/Scripts/LevelGenerator.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelGenerator : MonoBehaviour
+{
+
+    
+    public static LevelGenerator sharedInstance;                            // singletone variable
+    public List<LevelBlock> allTheLevelBlocks = new List<LevelBlock>();     // List of block which we will make a level
+    public Transform levelStartPoint;                                       // point where the nivel has to make itself
+    public List<LevelBlock> currentBlocks = new List<LevelBlock>();         // List of blocks that are currently in the scene
+
+    private void Awake()
+    {
+        sharedInstance = this;
+    }
+
+    public void AddLevelBlock()
+    {
+
+    }
+
+    public void RemoveOlvestLevelBlock()
+    {
+
+    }
+
+    public void RemoveAllTheBlocks()
+    {
+
+    }
+
+    public void GenerateInitialBlocks()
+    {
+
+    }
+}
